bd41ed76bc93fdaaffffd6b126b9b9ae7ae8d77b
alfiemartin <76059804+alfiemartin@users.noreply.github.com>
2021-02-09 16:36:00 +0000

    preload ?

--- components/Template.tsx
+++ components/Template.tsx
@@ -7,6 +7,10 @@ const Template = ({ children }) => {
       <Head>
         <title>Alfie Martin</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preload" href="naclogo.png" as="image"></link>
+        <link rel="preload" href="rdmlogo.gif" as="image"></link>
+        <link rel="preload" href="TapFlash logo.png" as="image"></link>
+        <link rel="preload" href="neurifylogo.png" as="image"></link>
       </Head>
       <main>{children}</main>
     </div>
